7e4122a0c941fe3f21e431d88a1cae0163eda22f
DanielMesfin <danielmesfin331@gmail.com>
2025-07-22 09:31:28 +0300

    this is working code

--- lib/common/config/http_config.dart
+++ lib/common/config/http_config.dart
@@ -1,6 +1,7 @@
 class HttpConfig {
-  static const String baseUrl = "https://kbackend-zxbb.onrender.com/";
   // static const String baseUrl = "http://192.168.68.224:3001/";
+  static const String baseUrl = "https://kbackend-yihi.onrender.com/";
+
   static const String platformUrl = '$baseUrl/api/plankton/completed';
   static const String imageUrl = '$baseUrl/images/';
   static const String serviceImageUrlFolder = 'service';
@@ -10,8 +11,8 @@ class HttpConfig {
   static const String categoryImageUrlFolder = 'category';
   static const String productImageUrlFolder = 'product';
   static const String apiBaseUrl = "$baseUrl/api/";
-  static const int defaultConnectionTimeOut = 10 * 1000; //30 sec
-  static const int longConnectionTimeOut = 60 * 1000; //60 sec
-  static const int defaultReceiveTimeOut = 10 * 1000; //30 sec
-  static const int longReceiveTimeOut = 60 * 1000; //60 sec
+  static const int defaultConnectionTimeOut = 10 * 1000; 
+  static const int longConnectionTimeOut = 60 * 1000; 
+  static const int defaultReceiveTimeOut = 10 * 1000; 
+  static const int longReceiveTimeOut = 60 * 1000; 
 }
